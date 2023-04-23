5dab99c0571655771883c081ee3678e678af7c6d
DESKTOP-8BAC7IL\pak92 <pak926@nate.com>
2023-04-23 14:09:00 +0900

    FEAT. 댓글 전체 읽기 구현 - Get 방식 Comment 테이블의 전체 컬럼 읽기 - id 컬럼으로 정렬

--- src/main/java/com/kafka/springboot/sys/artisthome/ArtisthomeService.java
+++ src/main/java/com/kafka/springboot/sys/artisthome/ArtisthomeService.java
@@ -1,6 +1,7 @@
 package com.kafka.springboot.sys.artisthome;
 
 import com.kafka.springboot.sqlmap.model.dto.Artist;
+import com.kafka.springboot.sqlmap.model.dto.Comment;
 
 import java.util.List;
 
@@ -9,5 +10,7 @@ public interface ArtisthomeService {
     public Artist getartistinfobyid(int artistid);
 
     public List<Artist> getAllArtistinfo();
+
+    public List<Comment> getAllComment();
 
 }
